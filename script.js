0ca37af6ecac0fa395be76f0b6256ae66c361ab8
Matt Cudzik <mattcudzikpl@gmail.com>
2020-03-26 18:49:17 +0100

    26.03

---
+++ script.js
@@ -0,0 +1,11 @@
+//background img changer
+for(var i=1;i<10;i++){
+  document.querySelector(".bgPhoto").innerHTML="<img src=\"database/slideshow/[i].jpg\""
+}
+
+setInterval(imgChanger, 1000)
+function imgChanger(){
+
+
+
+}
